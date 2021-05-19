3b3ccd55f3fcf6a6df838161e11bb3c24b855653
Akshitha-Ajalade <81165727+Akshitha-Ajalade@users.noreply.github.com>
2021-05-19 15:31:49 +0530

    updated code

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import DigitalClock from "./DigitalClock";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <DigitalClock />
+      </>
+    );
+  }
 }
-
 
 export default App;
