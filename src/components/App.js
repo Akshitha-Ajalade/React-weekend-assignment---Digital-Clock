9a68ab20aab630d7b0e0f215dce3aa723c465915
Akshitha-Ajalade <81165727+Akshitha-Ajalade@users.noreply.github.com>
2021-05-19 16:40:26 +0530

    updated code

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-import DigitalClock from "./DigitalClock";
+// import DigitalClock from "./DigitalClock";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      time: new Date()
+    };
+  }
+
+  cTime() {
+    this.setState({ time: new Date() });
+  }
+
+  mount() {
+    this.interval = setInterval(() => {
+      this.cTime();
+    }, 1000);
+  }
+
+  clrTime() {
+    clearInterval(this.interval);
+  }
+
   render() {
     return (
-      <>
-        <DigitalClock />
-      </>
+      <div className="Clock">
+        <h1>Digital Clock</h1>
+        <h3 id="time">{this.state.time.toLocaleTimeString()}</h3>
+      </div>
     );
   }
 }
